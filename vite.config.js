4f820cfee059dde8f0703178a1eca60b25c7b25b
Rajudotin <balavenkatarajusingampalli@gmail.com>
2025-05-28 11:04:31 +0530

    Fix Vercel route issue: updated Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+//chaned the vite config file to use react and set the alias for src
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist',
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    port: 5173,
+  },
+  base: './',
+});
